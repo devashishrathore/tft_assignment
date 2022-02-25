eaa2c25d57cd5b204efe8e25ca8c58980d7aa247
devashishrathore <devrathod96445@gmail.com>
2022-02-25 21:44:03 +0530

    v2 code

--- src/tft.js
+++ src/tft.js
@@ -13,3 +13,16 @@ function addString(str) {
 }
 addString("1421")
 
+// input --> "1S5G11"
+// Output --> 8
+
+function addDigit(str) {
+    let newString = str.split("").map(Number).filter(x => !isNaN(x))
+    console.log(newString)
+    let output = 0
+    for (let i = 0; i < newString.length; i++) {
+        output += newString[i]
+    }
+    console.log(output)
+}
+addDigit("1S5G11")
